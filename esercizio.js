002bf6ee8aeb78a6e7c9f670697f7729f96239e5
giusymosca <giusymosca27@gmail.com>
2019-03-11 13:45:03 +0100

    salva

--- esercizio.js
+++ esercizio.js
@@ -3,5 +3,9 @@ const app=express();
 app.get('/',function (req,res){
 res.sendFile(__dirname + '/public/index.html');
 });
+
+app.get('/login',function (req,res){
+res.sendFile(__dirname + '/public/login.html');
+});
 app.listen(process.env.PORT || 3000);
 
